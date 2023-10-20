4f7c23c6259b0fae10247d5f9e799456366eb5ed
Kristina H <christigolub@gmail.com>
2023-10-20 03:30:11 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,82 @@
+import { Component } from 'react';
+import ContactList from './ContactList/ContactList';
+import ContactForm from './ContactForm/ContactForm';
+import { Container } from './index.styled';
+import Filter from './Filter/Filter';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  }
+
+  componentDidMount() {
+    const localData = localStorage.getItem('contacts');
+    if (localData) {
+      this.setState({ contacts: JSON.parse(localData) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    console.log('prevState', prevState);
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentWillUnmount() {
+    localStorage.removeItem('contacts');
+  }
+
+  addContactData = newContact => {
+    const isTrue = this.state.contacts.some(
+      ({ name }) => name === newContact.name
+    );
+    if (isTrue) {
+      Notify.failure(`${newContact.name} is already in contacts`);
+      return;
+    }
+    this.setState(({ contacts }) => ({
+      contacts: [newContact, ...contacts],
+    }));
+  };
+
+  removeContact = id => {
+    this.setState(prev => ({
+      contacts: prev.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  getFilterContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase().trim())
+    );
+  };
+
+  getFilterData = ({ target: { value } }) => {
+    this.setState({
+      filter: value,
+    });
+  };
+
+  render() {
+    const filterContacts = this.getFilterContacts();
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <ContactForm addContactData={this.addContactData} />
+        <h2>Contacts</h2>
+        <Filter filter={this.state.filter} getFilterData={this.getFilterData} />
+        <ContactList
+          contacts={filterContacts}
+          removeContact={this.removeContact}
+          getFilterContacts={this.getFilterContacts}
+        />
+      </Container>
+    );
+  }
+}
+
+export default App;
